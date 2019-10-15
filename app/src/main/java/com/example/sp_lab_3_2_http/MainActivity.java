00ef747f9e0586b4c87063bd95782a07c21eaa83
valerihaataja <valeri.haataja@hotmail.com>
2019-10-15 20:53:29 +0300

    commit 2

--- app/src/main/java/com/example/sp_lab_3_2_http/MainActivity.java
+++ app/src/main/java/com/example/sp_lab_3_2_http/MainActivity.java
@@ -3,35 +3,81 @@ package com.example.sp_lab_3_2_http;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-public class MainActivity extends AppCompatActivity implements NetworkThread.MyInterface, View.OnClickListener {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, NetworkThread.MyInterface {
     TextView textView;
     EditText editText;
     Button button;
+    public String osoite;
+    ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
         button = findViewById(R.id.button);
         editText = findViewById(R.id.editText);
-        textView = findViewById(R.id.textView);
         button.setOnClickListener(this);
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.INVISIBLE);
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+
+        osoite = editText.getText().toString();
+        StrictMode.ThreadPolicy policy =
+                new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        try{
+
+            URL url = new URL(osoite);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            InputStream in = new BufferedInputStream(connection.getInputStream());
+            String htmlString = convertStreamToString(in);
+            textView.setText(htmlString);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressBar.setVisibility(View.INVISIBLE);
+        Toast.makeText(this, "OnResume", Toast.LENGTH_SHORT).show();
+
+    }
+
+
+
+    String convertStreamToString(java.io.InputStream is) {
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
+
     }
 
     @Override
     public void palauta(String palautus) {
-        textView.setText(palautus);
-    }
 
-    @Override
-    public void onClick(View v) {
-        NetworkThread n = new NetworkThread(this, editText.getText().toString());
-        n.start();
     }
 }
--- app/src/main/java/com/example/sp_lab_3_2_http/NetworkThread.java
+++ app/src/main/java/com/example/sp_lab_3_2_http/NetworkThread.java
@@ -1,5 +1,8 @@
 package com.example.sp_lab_3_2_http;
 
+import android.view.View;
+import android.widget.ProgressBar;
+
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
@@ -9,7 +12,7 @@ import java.lang.*;
 public class NetworkThread extends Thread {
     String osoite;
     String palautus;
-
+    MainActivity main = new MainActivity();
 
     public interface MyInterface {
         void palauta(String palautus);
@@ -23,14 +26,14 @@ public class NetworkThread extends Thread {
     MyInterface callBackInterface = null;
 
     public void run() {
-        try {
-            URL url = new URL(osoite);
-            URLConnection urlConnection = url.openConnection();
-            InputStream inputStream = urlConnection.getInputStream();
-            Scanner scanner = new Scanner(inputStream).useDelimiter("\\A");
-            palautus = scanner.hasNext() ? scanner.next() : "";
 
-            callBackInterface.palauta(palautus);
+        try {
+                 URL url = new URL(osoite);
+                URLConnection urlConnection = url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                Scanner scanner = new Scanner(inputStream).useDelimiter("\\A");
+                palautus = scanner.hasNext() ? scanner.next() : "";
+                callBackInterface.palauta(palautus);
         }catch (Exception e) {
             e.printStackTrace();
         }
